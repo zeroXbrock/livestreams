8d37702373db4ffdcdea4fed8db8a0143f98ea0b
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-06-25 14:51:28 -0700

    base contract

--- suapp/src/Counter.sol
+++ suapp/src/Counter.sol
@@ -1,14 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Suave} from "suave-std/suavelib/Suave.sol";
+
 contract Counter {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    event NumberSet(uint256 number);
+
+    modifier confidential() {
+        require(Suave.isConfidential(), "Counter: not confidential");
+        _;
     }
 
-    function increment() public {
-        number++;
+    function onSetNumber(uint256 newNumber) public {
+        number = newNumber;
+        emit NumberSet(newNumber);
+    }
+
+    function setNumber() public confidential returns (bytes memory) {
+        bytes memory data = Suave.confidentialInputs();
+        uint256 newNumber = abi.decode(data, (uint256));
+        return abi.encodeWithSelector(this.onSetNumber.selector, newNumber);
     }
 }
